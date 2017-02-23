d564d826406c58e0f19d8bc5b56fae01167443ea
Mitra Ardron <mitra@mitra.biz>
2017-02-23 09:08:05 -0800

    Installation

--- dweb/install.sh
+++ dweb/install.sh
@@ -9,6 +9,8 @@ fVERBOSE=true
 PIPINSTALLS="py-dateutil pycrypto"
 #OTHER MODULES USE OFTEN : pytz CherryPy lxml xlwt py-dateutil phonenumbers Py-dateutil requests-mock passlib pycrypto"
 PIPEGREP='py-dateutil|pycrypto'
+DIRS="../cache ../cache/signedby"
+#GROUP="wheel"  # Define if want all checkAndCreate to use group
 
 if ${fPIPUPGRADE}
 then
@@ -22,3 +24,47 @@ then
 	#pip install xslt -U # This doesnt seem to exist any more, there was a note about requiring it, but maybe not needed
 fi
 ${fVERBOSE} && pip list --format=columns | egrep ${PIPEGREP}
+
+checkAndCreate() {
+    # Check for existance of diretory $1,
+    # if doesnt exist then create and make g+rw and group=luminutes
+    # make sure all directories and files in it are also g+w and luminutes
+    if [ ! -d "$1" ]
+    then
+        if ${fMAKECHANGE}
+        then
+            ${fVERBOSE} && echo "Making $1"
+            mkdir "$1"
+            if [ -n "${GROUP}" ]
+            then
+                echo "GROUPING"
+                chgrp "${GROUP}" "$1"
+                chmod g+rw "$1"
+            fi
+        else
+            echo "Need to make $1, with group ${GROUP} and perms g+w"
+        fi
+    else
+    	true
+        #${fVERBOSE} && echo -e "$1 exists"
+    fi
+    if [ -d "$1" ]
+    then
+        if ${fMAKECHANGE}
+        then
+            #echo "Fixing perms if needed"
+            if [ -n "${GROUP:=}" ]
+            then
+                echo "GROUPING"
+                sudo chgrp -R "${GROUP}" $1
+                sudo chmod -R g+rw $1
+            fi
+	else
+		# List bad ones if not making change
+        	find $1 ! -group "${GROUP}" -o ! -perm -g=w -ls
+        fi
+    fi
+}
+
+checkAndCreate "../cache"
+checkAndCreate "../cache/signedby"
