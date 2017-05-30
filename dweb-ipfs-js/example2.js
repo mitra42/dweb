24010cfd738c2a0bf87e608f0c6428368edc9187
Mitra Ardron <mitra@mitra.biz>
2017-05-29 20:08:01 -0700

    IPFS added example for pubsub - note EXPERIMENTAL.pubsub=true

--- dweb-ipfs-js/example2.js
+++ dweb-ipfs-js/example2.js
@@ -1,11 +1,12 @@
 // This example is to give a simple set of tests that incrementally run through the APIs I'm expecting to use for Dweb
 'use strict';
 
-// Other pagages
+// Other packages
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
-const promisify = require('promisify-es6');
+const promisify = require('promisify-es6'); //TODO remove as obsoleted by makepromises
+
 
 // IPFS packages
 const CID = require('cids');                // Content IDs
@@ -13,7 +14,9 @@ const Block = require('ipfs-block');        // Blocks (hash + data)
 const multihash = require('multihashes');   // Multihash - binary representation including type of hash
 const IPFS = require('ipfs');               // This might obviate need for some of the others, but not sure.
 
-
+// Utility packages (ours)
+const makepromises = require('./utils/makepromises')
+// Dweb packages (ours)
 
 /*
  * Create a new IPFS instance, using default repo (fs) on default path (~/.ipfs)
@@ -21,14 +24,29 @@ const IPFS = require('ipfs');               // This might obviate need for some 
 //const repodir = path.join(os.tmpdir() + '/' + new Date().toString())
 const repodir = '/tmp/ipfs' + Math.random()
 console.log("Storing to",repodir);
-const ipfs = new IPFS({
+
+
+let ipfs
+// (Earlier version and example1 Rewritten as a promise with promisify inline
+
+
+ipfs = new IPFS({
     repo: repodir,
     init: false,
     start: false,
     EXPERIMENTAL: {
-        pubsub: false
+        pubsub: true
     }
 })
+
+// Note many (but not all) ipfs functionality already promisify's as of (as of 2017-05-27)
+// DO: version, start, init, dag.*, pubsub
+// DONT block.*
+const promises_needed = [ { block: ["put", "get"] }]
+const promisified = {ipfs:{}}
+makepromises(ipfs, promisified.ipfs, promises_needed); // Has to be after ipfs defined
+//console.log(promisified.ipfs);
+
 
 
 // (Earlier version and example1 Rewritten as a promise with promisify inline
@@ -36,10 +54,10 @@ const ipfs = new IPFS({
 // This chunk starts up IPFS
 function start() {
     return new Promise((resolve, reject) => {
-        promisify(ipfs.version)()
+        ipfs.version()
             .then((version) => console.log("Version=", version))
-            .then((unused) => promisify(ipfs.init)({emptyRepo: true, bits: 2048}))
-            .then((unused) => promisify(ipfs.start)())
+            .then((unused) => ipfs.init({emptyRepo: true, bits: 2048}))
+            .then((unused) => ipfs.start())
             .then((unused) => {
                 if (ipfs.isOnline()) console.log('IPFS node is now ready and online');
                 resolve("MY MESSAGE");
@@ -58,14 +76,14 @@ function blocktest(message) {
     let cid;    // Holds id of block stored
     return new Promise((resolve, reject) => {
         console.log("MESSAGE=",message)
-        promisify(ipfs.block.put)(blob)
+        promisified.ipfs.block.put(blob)
             .then((block) => {
                 //console.log(block);
                 let cid = block.cid;
                 //console.log("CID=",cid);
                 return cid;
             })
-            .then((cid) => promisify(ipfs.block.get)(cid))
+            .then((cid) => promisified.ipfs.block.get(cid))
             .then((result) => {
                 //console.log(result);
                 let data = result.data.toString();
@@ -83,9 +101,9 @@ function dagtest(message) {
     let mydata = { name: 'Foo Bar', address: 'last shack on right'}
     return new Promise((resolve, reject) => {
         console.log("Starting dagtest",message);
-        promisify(ipfs.dag.put)(mydata, {format: 'dag-cbor', hashAlg: 'sha2-256'})
+        ipfs.dag.put(mydata, {format: 'dag-cbor', hashAlg: 'sha2-256'})
         //.then((cid) => { console.log("DAG PUT=",cid); return(cid); })
-            .then((cid)=>promisify(ipfs.dag.get)(cid))
+            .then((cid)=>ipfs.dag.get(cid))
             .then((result)=>{
                 //console.log(result.value);
                 console.assert(result.value.name === mydata.name, "DAG should survive roundtrip");
@@ -99,11 +117,44 @@ function dagtest(message) {
 }
 
 
-//Serial
-//start().then((res) => blocktest(res)).then((unused) => dagtest(unused))
 
-// or Parallel
-start().then((unused) => Promise.all([ blocktest(""), dagtest("STARTING DAG") ]))
+// (Earlier version and example1 Rewritten as a promise with promisify inline
+
+const msg1 = "Did you hear that"
+const receiveMsg = (msg) => {
+    console.log("Subscribe received",msg.data.toString());
+    console.assert(msg.data.toString() === msg1, "Should hear msg1 back from publish")
+}
+
+
+// This chunk starts up IPFS
+function pubsubtest() {
+    return new Promise((resolve, reject) => {
+        let topic = "A place to gossip";
+        console.log("pubsubtest starting")
+        ipfs.pubsub.subscribe(topic, receiveMsg);   // Not a promise it sets up receiveMsg as a listener
+        ipfs.pubsub.publish(topic, new Buffer(msg1))
+            .then((unused) => {
+                if (ipfs.isOnline()) console.log('pubsubtest Exiting');
+                resolve(undefined);
+            })    //TODO throw error if not online
+            .catch((err) => {
+                console.log("UNCAUGHT ERROR in start", err);
+                reject(err)
+            })
+    })
+}
+
+
+//Serial just one
+//start().then((unused) => pubsubtest())
+
+//Serial all of them
+//start().then((res) => blocktest(res)).then((unused) => dagtest(unused)).then((unused) => pubsubtest())
+
+// or Parallel all of them
+start().then((unused) => Promise.all([ blocktest(""), dagtest("STARTING DAG"), pubsubtest() ]))
+
 
 /* Pending input from David & Matt
  const cid = new CID(1, 'dag-pb', multihash);  //TODO- what is this ?
--- dweb-ipfs-js/example3.js
+++ dweb-ipfs-js/example3.js
@@ -1,20 +1,22 @@
 // This example is to give a simple set of tests that incrementally run through the APIs I'm expecting to use for Dweb
 'use strict';
 
-// Other pagages
+// Other packages
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
-// Note many (but not all) ipfs functionality already promisify's
-// DO: dag.*; version
-// DONT start, block.*
-const promisify = require('promisify-es6');
+const promisify = require('promisify-es6'); //TODO remove as obsoleted by makepromises
+
 
 // IPFS packages
 const CID = require('cids');                // Content IDs
 const Block = require('ipfs-block');        // Blocks (hash + data)
 const multihash = require('multihashes');   // Multihash - binary representation including type of hash
 const IPFS = require('ipfs');               // This might obviate need for some of the others, but not sure.
+
+// Utility packages (ours)
+const makepromises = require('./utils/makepromises')
+// Dweb packages (ours)
 
 /*
  * Create a new IPFS instance, using default repo (fs) on default path (~/.ipfs)
@@ -26,43 +28,36 @@ console.log("Storing to",repodir);
 
 let ipfs
 // (Earlier version and example1 Rewritten as a promise with promisify inline
-let promisified = {ipfs:{}}
-const promises_needed = [ "start", block: ["put", "get"], "":["start"]}
-function makepromises(p, pn) {
-    for (let i in pn) {
-        if (typeof i === "integer) {
-            p[pn[i]
-        }
+
+
+ipfs = new IPFS({
+    repo: repodir,
+    init: false,
+    start: false,
+    EXPERIMENTAL: {
+        pubsub: true
     }
-    for (let i in promises_needed) {
-        if (i && !promisified.ipfs[i]) promisified.ipfs[i] = {};
-        for (let j in promises_needed[i]) {
-            let name = promises_needed[i][j]
-            console.log("J=",name)
-            promisified.ipfs[i][name] = promisify(ipfs[i][name]);
-        }
-    }
-    console.log("Promises",promisified)
-}
+})
+
+// Note many (but not all) ipfs functionality already promisify's as of (as of 2017-05-27)
+// DO: version, start, init, dag.*, pubsub
+// DONT block.*
+const promises_needed = [ { block: ["put", "get"] }]
+const promisified = {ipfs:{}}
+makepromises(ipfs, promisified.ipfs, promises_needed); // Has to be after ipfs defined
+//console.log(promisified.ipfs);
+
+
+
+// (Earlier version and example1 Rewritten as a promise with promisify inline
 
 // This chunk starts up IPFS
 function start() {
-    ipfs = new IPFS({
-        repo: repodir,
-        init: false,
-        start: false,
-        EXPERIMENTAL: {
-            pubsub: false
-        }
-    })
-    makepromises(promises_needed);
-    console.log(ipfs.promises);
-
     return new Promise((resolve, reject) => {
-        promisify(ipfs.version)()
+        ipfs.version()
             .then((version) => console.log("Version=", version))
-            .then((unused) => promisify(ipfs.init)({emptyRepo: true, bits: 2048}))
-            .then((unused) => promisify(ipfs.start)())
+            .then((unused) => ipfs.init({emptyRepo: true, bits: 2048}))
+            .then((unused) => ipfs.start())
             .then((unused) => {
                 if (ipfs.isOnline()) console.log('IPFS node is now ready and online');
                 resolve("MY MESSAGE");
@@ -74,27 +69,21 @@ function start() {
     })
 }
 
-
 // This chunk tests block storage
-// Note block.js isn't currently (2017-05-27) promisified
-
 function blocktest(message) {
-    // Note have to do this inside blocktest as otherwise ipfs is still undefined
-    let promise_ipfs_block_put = promisify(ipfs.block.put)
-    let promise_ipfs_block_get = promisify(ipfs.block.get)
     let blobtxt = 'a serialized object'
     const blob = new Buffer(blobtxt);
     let cid;    // Holds id of block stored
     return new Promise((resolve, reject) => {
         console.log("MESSAGE=",message)
-        ipfs.promises.block.put(blob)
+        promisified.ipfs.block.put(blob)
             .then((block) => {
                 //console.log(block);
                 let cid = block.cid;
                 //console.log("CID=",cid);
                 return cid;
             })
-            .then((cid) => promise_ipfs_block_get(cid))
+            .then((cid) => promisified.ipfs.block.get(cid))
             .then((result) => {
                 //console.log(result);
                 let data = result.data.toString();
@@ -113,8 +102,8 @@ function dagtest(message) {
     return new Promise((resolve, reject) => {
         console.log("Starting dagtest",message);
         ipfs.dag.put(mydata, {format: 'dag-cbor', hashAlg: 'sha2-256'})
-        .then((cid) => { console.log("DAG PUT=",cid); return(cid); })
-            .then((cid)=>ipfs.dag.get(cid, null, null))
+        //.then((cid) => { console.log("DAG PUT=",cid); return(cid); })
+            .then((cid)=>ipfs.dag.get(cid))
             .then((result)=>{
                 //console.log(result.value);
                 console.assert(result.value.name === mydata.name, "DAG should survive roundtrip");
@@ -127,4 +116,47 @@ function dagtest(message) {
     })
 }
 
-start().then((unused) => Promise.all([ blocktest(""), dagtest("STARTING DAG") ]))
+
+
+// (Earlier version and example1 Rewritten as a promise with promisify inline
+
+const msg1 = "Did you hear that"
+const receiveMsg = (msg) => {
+    console.log("Subscribe received",msg.data.toString());
+    console.assert(msg.data.toString() === msg1, "Should hear msg1 back from publish")
+}
+
+
+// This chunk starts up IPFS
+function pubsubtest() {
+    return new Promise((resolve, reject) => {
+        let topic = "A place to gossip";
+        console.log("pubsubtest starting")
+        ipfs.pubsub.subscribe(topic, receiveMsg);   // Not a promise it sets up receiveMsg as a listener
+        ipfs.pubsub.publish(topic, new Buffer(msg1))
+        .then((unused) => {
+                if (ipfs.isOnline()) console.log('pubsubtest Exiting');
+                resolve(undefined);
+            })    //TODO throw error if not online
+            .catch((err) => {
+                console.log("UNCAUGHT ERROR in start", err);
+                reject(err)
+            })
+    })
+}
+
+
+//Serial just one
+//start().then((unused) => pubsubtest())
+
+//Serial all of them
+//start().then((res) => blocktest(res)).then((unused) => dagtest(unused)).then((unused) => pubsubtest())
+
+// or Parallel all of them
+start().then((unused) => Promise.all([ blocktest(""), dagtest("STARTING DAG"), pubsubtest() ]))
+
+
+/* Pending input from David & Matt
+ const cid = new CID(1, 'dag-pb', multihash);  //TODO- what is this ?
+ */
+
